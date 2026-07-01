db8dd1d9f937d76ab800901966248e57d5f64f75
calgal7-del <calgal7@live.ca>
2026-07-01 16:37:36 -0600

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,32 @@
+export const DEFAULT_FORM = {
+  startingBalance: '25000',
+  contribution: '1000',
+  frequency: 'Biweekly',
+  currency: 'CAD',
+  currentAge: '35',
+  retireAge: '65',
+  expectedReturn: '7.0',
+  inflation: '2.0',
+  contributionIncrease: '0',
+  managementFees: '0.5',
+  retirementTaxRate: '20',
+  governmentBenefit: '0',
+  employerPension: '0',
+  withdrawalYears: '20',
+}
 
+export const PAY_FREQUENCIES = {
+  Daily: 365,
+  Weekly: 52,
+  Biweekly: 26,
+  'Semi-monthly': 24,
+  Monthly: 12,
+  Quarterly: 4,
+  Yearly: 1,
+}
+
+export const STORAGE_KEYS = {
+  journal: 'paydays.journal.v2',
+  currency: 'paydays.currency.v2',
+  form: 'paydays.form.v2',
+}
