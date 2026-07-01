afd132c81e8b2f7a0f5b34fd91758971020f4937
calgal7-del <calgal7@live.ca>
2026-07-01 16:48:46 -0600

    Update useProjection.js

--- src/hooks/useProjection.js
+++ src/hooks/useProjection.js
@@ -1,1 +1,33 @@
+import { useMemo } from 'react'
+import { buildProjection } from '../utils/projection'
+import {
+  buildGraphCallouts,
+  buildGraphData,
+  buildMilestones,
+} from '../utils/graphBuilder'
 
+export function useProjection(form) {
+  const projection = useMemo(() => buildProjection(form), [form])
+
+  const graphData = useMemo(
+    () => buildGraphData(projection),
+    [projection]
+  )
+
+  const milestones = useMemo(
+    () => buildMilestones(projection),
+    [projection]
+  )
+
+  const callouts = useMemo(
+    () => buildGraphCallouts(projection),
+    [projection]
+  )
+
+  return {
+    projection,
+    graphData,
+    milestones,
+    callouts,
+  }
+}
