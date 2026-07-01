6f4c528585bdf66e0ae9b677fc5a1b99831ca40b
calgal7-del <calgal7@live.ca>
2026-07-01 16:48:18 -0600

    Update useCurrency.js

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -1,1 +1,19 @@
+import { useMemo } from 'react'
+import { CURRENCIES, getCurrency } from '../utils/currencies'
 
+export function useCurrency(currencyCode) {
+  const currency = useMemo(
+    () => getCurrency(currencyCode),
+    [currencyCode]
+  )
+
+  return {
+    currency,
+    currencies: CURRENCIES,
+    symbol: currency.symbol,
+    code: currency.code,
+    flag: currency.flag,
+    locale: currency.locale,
+    country: currency.country,
+  }
+}
