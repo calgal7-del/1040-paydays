3d0b6ac3cdddb0caf145ef38d05056337a825f52
calgal7-del <calgal7@live.ca>
2026-07-01 22:59:37 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import logo from "../assets/logo.svg";
 
-function Footer() {
+export default function Footer() {
   return (
     <footer className="siteFooter">
       <div className="footerLogo">
@@ -11,19 +11,6 @@ function Footer() {
       <div className="footerPill">↗ Build consistency</div>
       <div className="footerPill">◎ Future-focused planning</div>
 
-      <div className="footerBottom">
-        © {new Date().getFullYear()} 1040 Paydays.
-      </div>
-    </footer>
-  );
-}
-export function Footer() {
-  return (
-    <footer className="siteFooter">
-      <div className="footerLogo">1040 Paydays</div>
-      <div className="footerPill">○ One payday at a time</div>
-      <div className="footerPill">↗ Build consistency</div>
-      <div className="footerPill">◎ Future-focused planning</div>
       <div className="footerBottom">
         © {new Date().getFullYear()} 1040 Paydays.
       </div>
